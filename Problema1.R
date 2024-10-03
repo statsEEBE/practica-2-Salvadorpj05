101b7fd3aeb83dabadca09dd5b9274fefad41757
salvador.plaixens <salvador.plaixens@auleseebe.upc.edu>
2024-10-03 13:23:20 +0200

    LAB 2

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,11 @@
 #Codigo para problema 1
 
+#coef de determinacio 0.1923 r^2
+#coef de correlacio 0.4385202 r
+sqrt(0.1923)
+
+m <- 0.0676
+b <- 11.79755
+x <- 647.1
+y_pred <- m*x
+
--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,45 @@
 #Codigo para problema 2
+
+mis_dades <- iris
+mis_dades
+
+x <- mis_dades$Petal.Length
+x
+
+y <- mis_dades$Sepal.Length
+y
+
+plot(x,y)
+
+x_bar <- mean(x)
+x_bar
+
+y_bar <- mean(y)
+y_bar
+
+m <- sum((x-x_bar)*(y-y_bar))/sum((x-x_bar)^2)
+m
+
+b <- y_bar - m*x_bar
+b
+
+m*1.5+b
+
+#perdiccio sobre les observacions
+y_pred <- m*x+b
+
+plot(x, y)
+lines(x, y_pred)
+
+R_sq <- sum((y_pred-y_bar)^2)/sum((y-y_bar)^2)
+R_sq
+
+######Usando las funciones de R
+#~ == ALT GR + 4 + ESPAI
+
+mod <- lm(y~x)
+mod
+
+summary(mod)
+
+cor.test(x,y)
